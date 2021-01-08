91c68fd7160bca84312ca7ecac2c6a70f8a491a4
shaokun <605416017@qq.com>
2021-01-08 15:05:35 +0800

    1/使用css方式控制svg,方便复用 2/整理router的规则

--- app.js
+++ app.js
@@ -1,60 +1,56 @@
 // css 转义
 // http://tool.mkblog.cn/autoprefixer/
-var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+
+// svg 通过img引入并更改颜色 https://codepen.io/sosuke/pen/Pjoqqp
+var toastElList = [].slice.call(document.querySelectorAll(".toast"));
 var toastList = toastElList.map(function (toastEl) {
-  return new bootstrap.Toast(toastEl)
-})
+  return new bootstrap.Toast(toastEl);
+});
 var app = new Vue({
   el: "#app",
   created: function () {
-    let ws  =new WebSocket("ws://127.0.0.1:30400")
+    // let ws = new ReconnectingWebSocket("ws://127.0.0.1:30400");
     let that = this;
     const router = new VueRouter(function () {
-      console.log("not found");
+      that._switchPage("main");
       that.switchRouter("main");
     });
+    this.routerCtrl = router;
     router.route("/main", function () {
-      that._switchRouter("main");
+      that._switchPage("main");
       console.log("main");
     });
     router.route("/tx/:id", function (params) {
-      that._switchRouter("tx");
+      that._switchPage("tx");
       let txID = params.slice(params.lastIndexOf("/") + 1);
       console.log("tx id is ", txID);
     });
     router.route("/name", function () {
-      that._switchRouter("name");
+      that._switchPage("name");
       console.log("router name");
     });
   },
   data: {
-    router: {
+    pages: {
       main: true,
       tx: false,
       name: false,
     },
     message: "Hello shaokun",
+    routerCtrl: null,
   },
   methods: {
-    _switchRouter: function (action) {
-      for (let route of Object.keys(this.router)) {
-        if (route === action) {
-          this.router[route] = true;
+    switchRouter: function (action, parmas) {
+      this.routerCtrl.switchRouter(action, parmas);
+    },
+    _switchPage: function (_page) {
+      for (let page of Object.keys(this.pages)) {
+        if (page === _page) {
+          this.pages[page] = true;
         } else {
-          this.router[route] = false;
+          this.pages[page] = false;
         }
       }
-    },
-
-    switchRouter: function (action = "main", params = "") {
-      toastr.info('hello shaokun')
-      let url = !!action ? "#/" + action : "";
-      if (params) url = url + "/" + params;
-      var a = document.createElement("a");
-      a.setAttribute("href", url);
-      a.setAttribute("target", "_self");
-      a.setAttribute("id", "router_id_x");
-      a.click();
     },
   },
 });
