89d66cafbae577b7cf1195eaf8e2beb2269d3fdd
shaokun11 <605416017@qq.com>
2021-05-13 23:31:44 +0800

    增加常用lib 路由目前不支持查询 待优化

--- app.js
+++ app.js
@@ -4,12 +4,45 @@
 // svg 通过img引入并更改颜色 https://codepen.io/sosuke/pen/Pjoqqp
 
 // js 压缩  https://tool.lu/js/index.html
-var app = new Vue({
+function handleWs(_this) {
+  // let rws = new ReconnectingWebSocket("ws://127.0.0.1:8000");
+  // rws.addEventListener("open", () => {
+  //   rws.send(JSON.stringify({
+  //     type: "login",
+  //     data: {
+  //       user: "shaokun",
+  //       password: "1234"
+  //     }
+  //   }));
+  //   rws.send(JSON.stringify({
+  //     type:"loginWithToken",
+  //     data:{
+  //       token:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50Ijoic2hhb2t1biIsImlhdCI6MTYxMjE1MTE5OCwiZXhwIjoxNjEyMTU0Nzk4fQ.XUCcHj8Sav1P8ModA-1iRZBp9bxxJSWuVcRE1f0iSKw"
+  //     }
+  //   }))
+  //
+  //   setTimeout(() => {
+  //     rws.send(JSON.stringify({
+  //       type: "getMe",
+  //       data: {}
+  //     }));
+  //   }, 1000);
+  // });
+  // rws.addEventListener("close", () => {
+  //   console.log("close");
+  // });
+  // rws.addEventListener("message", (message) => {
+  //   console.log("message", JSON.parse(message.data));
+  // });
+}
+
+
+new Vue({
   el: "#app",
   created: function () {
-    // let ws = new ReconnectingWebSocket("ws://127.0.0.1:30400");
+    handleWs(this);
     let that = this;
-    const router = new VueRouter(function () {
+    const router = new VueRouter(function (params) {
       that._switchPage("main");
       that.switchRouter("main");
     });
@@ -32,10 +65,10 @@ var app = new Vue({
     pages: {
       main: true,
       tx: false,
-      name: false,
+      name: false
     },
     message: "Hello shaokun",
-    routerCtrl: null,
+    routerCtrl: null
   },
   methods: {
     switchRouter: function (action, params) {
@@ -45,6 +78,6 @@ var app = new Vue({
       for (let page of Object.keys(this.pages)) {
         this.pages[page] = page === _page;
       }
-    },
-  },
+    }
+  }
 });
---
+++ readme.md
@@ -0,0 +1,4 @@
+###简单网页的书写
+> 可以结合自定义
+  [bootsrap](https://github.com/shaokun11/bootsrap5-theme)
+自定义主题
